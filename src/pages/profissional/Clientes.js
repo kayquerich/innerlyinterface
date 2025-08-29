6fd55bbf853322bc277c944a00f03e45382837ec
kayqueR69 <kayquericharlyson@gmail.com>
2025-08-29 09:50:45 -0300

    fix: ajustes de responsividade finalizados

--- src/pages/profissional/Clientes.js
+++ src/pages/profissional/Clientes.js
@@ -1,10 +1,11 @@
 import styles from '../../styles/styles-profissional/clientes.module.css'
 import { InternalPage as Page } from "../../components/Container"
-import { Separator, Subtitle } from "../../components/Text"
+import { Subtitle } from "../../components/Text"
 import { ClientPreview } from "../../components/Card"
 import { useEffect, useState } from 'react'
 import { listarClientes, listarRegistrosCliente } from '../../services/Profissionais'
 import { Registro } from '../../components/Registro'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 
 export default function Clientes () {
 
@@ -12,6 +13,7 @@ export default function Clientes () {
 
     const [clientes, setClientes] = useState([]) 
     const [registros, setRegistros] = useState([])
+    const [largura, setLargura] = useState(window.innerWidth)
 
     useEffect(() => {
 
@@ -36,52 +38,69 @@ export default function Clientes () {
         }
         preencherEstado()
 
+        const handleResize = () => setLargura(window.innerWidth)
+        window.addEventListener('resize', handleResize) 
+        return () => window.removeEventListener('resize', handleResize)
+
     }, [])
+
+    const [client_view, setClient_View] = useState(true)
+    const clientClick = () => {
+        setClient_View(!client_view)
+    }
 
     const buscarRegistros = async (id) => {
         const response = await listarRegistrosCliente(profissional.token, id)
         sessionStorage.setItem('temporary', JSON.stringify(response))
         setRegistros(response)
+        clientClick()
     }
 
     return (
         <Page dados={profissional} style={{ display : 'flex' }} >  
+            <div className={styles.page} >
 
-            <div className={styles.container_clientes} >
-                <Subtitle>Clientes</Subtitle>
-                <Separator margin={20} />
+                <div className={styles.container_clientes} style={(!client_view && largura < 940) ? {display : 'none'} : {}} >
+                    <Subtitle>Clientes</Subtitle>
 
-                <div className={styles.lista_clientes} >
-                    {clientes.map((item, index) => (
-                        item.is_ativo && <ClientPreview 
-                            dados={item} 
-                            key={index}
-                            execute={() => buscarRegistros(item.id)} 
-                        />
-                    ))}
-                </div>
-
-            </div>
-
-            <div className={styles.container_registros} >
-                <Subtitle>Registros</Subtitle>
-
-                {!registros.length && (
-                    <div className={styles.void_container} >
-                        <p>Clique em um dos clientes para apresentar seus registros</p>
-                    </div>
-                )}
-
-                {registros.length && (
-                    <div className={styles.lista_registros} >
-                        {registros.map((item, index) => (
-                            <Registro dados={profissional} registro={item} key={index} />
+                    <div className={styles.lista_clientes} >
+                        {clientes.map((item, index) => (
+                            item.is_ativo && <ClientPreview 
+                                dados={item} 
+                                key={index}
+                                execute={() => buscarRegistros(item.id)} 
+                            />
                         ))}
                     </div>
+
+                </div>
+
+                <div className={styles.container_registros} style={(client_view && largura < 940) ? {display : 'none'} : {}}>
+                    <Subtitle>Registros</Subtitle>
+
+                    {!registros.length && (
+                        <div className={styles.void_container} >
+                            <p>Clique em um dos clientes para apresentar seus registros</p>
+                        </div>
+                    )}
+
+                    {registros.length && (
+                        <div className={styles.lista_registros} >
+                            {registros.map((item, index) => (
+                                <Registro dados={profissional} registro={item} key={index} />
+                            ))}
+                        </div>
+                    )}
+
+                </div>
+
+                {(!client_view && largura < 940) && (
+                    <div className={styles.back_button} onClick={clientClick} >
+                        <Icon icon="chevron-left" />
+                    </div>
                 )}
 
             </div>
-
         </Page>
     )
 
